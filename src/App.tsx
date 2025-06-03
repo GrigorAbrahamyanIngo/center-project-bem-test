dbbfc8bd1867163f9ca3fcd802c62c25be2d9bc3
grigorabrahamyan <gripgor.abrahamyan@ingoarmenia.am>
2025-06-03 13:50:45 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
